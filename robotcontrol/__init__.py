cb6c43dac80916ff8593ce760cced76553c38150
bertoramos <ramossanchezalberto@gmail.com>
2025-12-22 11:17:42 +0000

    doc: update 1.9 version

--- robotcontrol/__init__.py
+++ robotcontrol/__init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "Robotcontrol",
     "author": "Alberto Ramos Sanchez",
-    "version": (1, 8, 1),
+    "version": (1, 9, 0),
     "blender": (2, 80, 0),
     "location": "View3D > Sidebar Panel > Archibuilder Panel",
     "description": "Robot communication module",
